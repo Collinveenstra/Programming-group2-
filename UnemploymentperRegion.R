8cc394e875bad066b14c164f06b85613a72e1562
valenwcaruso <valenwcaruso@gmail.com>
2025-06-10 14:48:59 +0200

    Update UnemploymentperRegion.R

--- UnemploymentperRegion.R
+++ UnemploymentperRegion.R
@@ -1,1 +1,3 @@
-Arbeidsmarktsituatie_jongeren__regio_10062025_142603 <- read.csv("~/Downloads/Arbeidsmarktsituatie_jongeren__regio_10062025_142603.csv", sep=";")
+UnemploymentRegion <- read.csv("~/Downloads/Arbeidsmarktsituatie_jongeren__regio_10062025_142603.csv", sep=";")
+UnemploymentRegion$Geregistreerd.werkzoekende.bij.UWV <- as.factor(UnemploymentRegion$Geregistreerd.werkzoekende.bij.UWV)
+summary(UnemploymentRegion$Geregistreerd.werkzoekende.bij.UWV)

0ebc88cb2525b8abdc5c69ac9d33e1199febf906
valenwcaruso <valenwcaruso@gmail.com>
2025-06-10 15:07:30 +0200

    Added data of unemployment by region

--- UnemploymentperRegion.R
+++ UnemploymentperRegion.R
@@ -1,3 +1,5 @@
 UnemploymentRegion <- read.csv("~/Downloads/Arbeidsmarktsituatie_jongeren__regio_10062025_142603.csv", sep=";")
 UnemploymentRegion$Geregistreerd.werkzoekende.bij.UWV <- as.factor(UnemploymentRegion$Geregistreerd.werkzoekende.bij.UWV)
 summary(UnemploymentRegion$Geregistreerd.werkzoekende.bij.UWV)
+
+write_csv(UnemploymentRegion, "data/unemployment.csv")

18c4dce6219c6c9f7152a69ae200a4b5fdda3351
Collinveenstra <collinveenstra@hotmail.com>
2025-06-16 15:07:59 +0200

    Create gem_unemployment.R

---
+++ gem_unemployment.R
@@ -0,0 +1,3 @@
+gem_unemployment <- read.csv("C:/Users/Collin Veenstra/Downloads/Arbeidsmarktsituatie_jongeren__regio_16062025_135806.csv", sep=";")
+View(gem_unemployment)
+write.csv(gem_unemployment,"standard data/gem_unemployment.csv")
